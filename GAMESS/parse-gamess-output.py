9f93796b8e848629e28361ea80bb536ff19b1206
Arnab Chakraborty <arnab.chakraborty999@gmail.com>
2025-01-16 22:54:34 -0800

    now extracts point group of the molecule too

--- GAMESS/parse-gamess-output.py
+++ GAMESS/parse-gamess-output.py
@@ -25,6 +25,20 @@ def get_ccsd(f):
             break
             
     return x[2],x[5]
+
+# Point Group
+def get_pointgroup(f):
+    for line in f:
+        if "THE POINT GROUP OF THE MOLECULE IS " in line:
+            x = line.split()
+        elif "THE ORDER OF THE PRINCIPAL AXIS IS " in line:
+            y = line.split()
+            break
+    
+    pg = x[7]
+    pg = pg.replace("N",y[7])
+     
+    return pg
                 
         
 def main():
@@ -34,22 +48,34 @@ def main():
         
     filename = sys.argv[1]
     
-    with open(filename, 'r') as f:
-        print("")
-        print("==== GAMESS OUTPUT PARSING SCRIPT ====")
-        print("")
-        print("Parsing the " + filename + " file.")
-        print("")
-        #  
-        # extract the reference energy
-        ref_e = get_reference(f)
-        print("REFERENCE ENERGY: "+ ref_e + " Hartree")
-        # 
-        # extract CCSD energy
-        ccsd_e, ccsd_corr = get_ccsd(f)
-        print("CCSD ENERGY:      "+ ccsd_e + " Hartree "+" CORR. E: " + ccsd_corr + " Hartree")
-        
+    try:
+        with open(filename, 'r', encoding='utf-8') as file:
+            f = file.readlines()
+    except FileNotFoundError:
+        print(f"Error: File not found - {filename}")
+        sys.exit(1)
+    except Exception as e:
+        print(f"An error occurred while reading the file: {e}")
+        sys.exit(1)
     
+    print("")
+    print("==== GAMESS OUTPUT PARSING SCRIPT ====")
+    print("")
+    print(f"Parsing the {filename} file.")
+    print("")
+    #  
+    # extract the reference energy
+    ref_e = get_reference(f)
+    print(f"REFERENCE ENERGY:  {ref_e} Hartree")
+    # 
+    # extract CCSD energy
+    ccsd_e, ccsd_corr = get_ccsd(f)
+    print(f"CCSD ENERGY:        {ccsd_e} Hartree  CORR. E: {ccsd_corr} Hartree")
+    #
+    # extract point group symmetry
+    pg = get_pointgroup(f) 
+    print(f"POINT GROUP SYMMETRY: {pg}")
+       
     
 if __name__ == "__main__":
     main()
